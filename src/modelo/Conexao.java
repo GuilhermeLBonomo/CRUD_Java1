0b8d8ace88a7c58b74966ece347afd837ca7f6cb
GuilhermeLBonomo <Darkish5044@proton.me>
2023-12-22 17:19:51 -0300

    Commit: 2023-12-22

--- src/modelo/Conexao.java
+++ src/modelo/Conexao.java
@@ -1,17 +1,20 @@
 package modelo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.*;
 
 public final class Conexao {
-    private static final String URL = "jdbc:mariadb://localhost:3306/seu_banco_de_dados";
     private static final String USUARIO = "seu_usuario";
     private static final String SENHA = "sua_senha";
+    private static final String DATABASE = "seu_banco_de_dados";
+    private static final String SERVER = "localhost";
+    private static final int PORTA = 3306;
+    private static final String URL = "jdbc:mariadb://" + SERVER + ":" + PORTA + "/" + DATABASE;
 
     public static Connection obterConexao() throws SQLException {
         try {
+            checarDatabase("");
             Class.forName("org.mariadb.jdbc.Driver");
             return DriverManager.getConnection(URL, USUARIO, SENHA);
         } catch (ClassNotFoundException e) {
@@ -36,23 +39,113 @@ public final class Conexao {
     }
 
 
-    public static boolean executarComandoSQL(String sql, Object... parametros) throws SQLException {
-        try (Connection conexao = obterConexao();
-             PreparedStatement ps = conexao.prepareStatement(sql)) {
+    public static boolean executarComandoSQL(final String database, final String tabela, final String sql, final Object... parametros) throws SQLException {
+        Connection conexao = null;
+        try {
+            String database_out = checarDatabase(database);
+            checarTabela(database_out, tabela);
 
-            for (int i = 0; i < parametros.length; i++) {
-                ps.setObject(i + 1, parametros[i]);
+            conexao = obterConexao();
+            try (PreparedStatement ps = conexao.prepareStatement(sql)) {
+                for (int i = 0; i < parametros.length; i++) {
+                    ps.setObject(i + 1, parametros[i]);
+                }
+
+                int linhasAfetadas = ps.executeUpdate();
+                return linhasAfetadas > 0;
             }
-
-            int linhasAfetadas = ps.executeUpdate();
-            return linhasAfetadas > 0;
-
         } catch (SQLException e) {
             System.err.printf("Erro SQL ao executar comando: %s%n", e.getMessage());
             return false;
         } catch (Exception e) {
             System.err.printf("Erro desconhecido ao executar comando: %s%n", e.getMessage());
             return false;
+        } finally {
+            fecharConexao(conexao);
+        }
+    }
+
+    public static ResultSet executarConsultaSQL(final String database, final String tabela, final String sql, final Object... parametros) throws SQLException {
+        Connection conexao = null;
+        ResultSet resultSet;
+        try {
+            conexao = obterConexao();
+            checarDatabase(database);
+            checarTabela(database, tabela);
+
+            try (PreparedStatement ps = conexao.prepareStatement(sql)) {
+                for (int i = 0; i < parametros.length; i++) {
+                    ps.setObject(i + 1, parametros[i]);
+                }
+
+                resultSet = ps.executeQuery();
+            }
+        } catch (SQLException e) {
+            System.err.printf("Erro SQL ao executar consulta: %s%n", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            System.err.printf("Erro desconhecido ao executar consulta: %s%n", e.getMessage());
+            throw new SQLException("Erro desconhecido ao executar consulta.", e);
+        } finally {
+            fecharConexao(conexao);
+        }
+        return resultSet;
+    }
+
+
+    public static @NotNull String checarDatabase(final String database) throws SQLException {
+        String database_out = database;
+        if (database == null || database.isEmpty()) {
+            database_out = Conexao.DATABASE;
+        }
+        Connection conexao = null;
+
+        try {
+            conexao = obterConexao();
+            String sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?";
+            try (PreparedStatement ps = conexao.prepareStatement(sql)) {
+                ps.setString(1, database_out);
+                ResultSet resultSet = ps.executeQuery();
+
+                if (!resultSet.next()) {
+                    System.err.println("O banco de dados '" + database_out + "' não existe.");
+                } else {
+                    System.out.println("O banco de dados '" + database_out + "' existe.");
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao checar o banco de dados: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.printf("Erro desconhecido ao executar comando: %s%n", e.getMessage());
+        } finally {
+            fecharConexao(conexao);
+        }
+        return database_out;
+    }
+
+    public static void checarTabela(final String database, final String tabela) throws SQLException {
+        Connection conexao = null;
+        try {
+            conexao = obterConexao();
+            String database_out = checarDatabase(database);
+            String sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?";
+            try (PreparedStatement ps = conexao.prepareStatement(sql)) {
+                ps.setString(1, database_out);
+                ps.setString(2, tabela);
+                ResultSet resultSet = ps.executeQuery();
+
+                if (!resultSet.next()) {
+                    System.err.println("A tabela '" + tabela + "' não existe no banco de dados '" + database_out + "'.");
+                } else {
+                    System.out.println("A tabela '" + tabela + "' existe no banco de dados '" + database_out + "'.");
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao checar a tabela: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.printf("Erro desconhecido ao executar comando: %s%n", e.getMessage());
+        } finally {
+            fecharConexao(conexao);
         }
     }
 
--- src/modelo/Fabricante.java
+++ src/modelo/Fabricante.java
@@ -1,10 +1,12 @@
 package modelo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import static modelo.Conexao.executarComandoSQL;
+import static modelo.Conexao.executarConsultaSQL;
 import static modelo.FuncoesUtilitarias.*;
 
 public final class Fabricante {
@@ -83,7 +85,7 @@ public final class Fabricante {
             return false;
         }
         String sql = "INSERT INTO Fabricante (FabricanteID, Nome, Localizacao, Descricao) VALUES (?, ?, ?, ?)";
-        return Conexao.executarComandoSQL(sql, fabricanteID, nome, localizacao, descricao);
+        return executarComandoSQL("", "Fabricante", sql, fabricanteID, nome, localizacao, descricao);
     }
 
     public boolean removerFabricante(final Integer fabricanteID) throws SQLException {
@@ -94,7 +96,7 @@ public final class Fabricante {
             return false;
         }
         final String sql = "DELETE FROM Fabricante WHERE FabricanteID = ?";
-        return Conexao.executarComandoSQL(sql, fabricanteID);
+        return executarComandoSQL("", "Fabricante", sql, fabricanteID);
     }
 
     public boolean atualizarFabricante(final Integer fabricanteID, final String novoNome, final String novaLocalizacao, final String novaDescricao) throws SQLException {
@@ -106,95 +108,53 @@ public final class Fabricante {
             return false;
         }
         final String sql = "UPDATE Fabricante SET Nome=?, Localizacao=?, Descricao=? WHERE FabricanteID=?";
-        return Conexao.executarComandoSQL(sql, novoNome, novaLocalizacao, novaDescricao, fabricanteID);
+        return executarComandoSQL("", "Fabricante", sql, novoNome, novaLocalizacao, novaDescricao, fabricanteID);
     }
 
-    public Fabricante consultarFabricantePorNome(String nome) throws SQLException {
-        Connection conexao = null;
-        PreparedStatement ps;
-        ResultSet rs;
 
+    public List<Fabricante> consultarFabricantesPorNome(String nome) throws SQLException {
+        List<Fabricante> fabricantes = new ArrayList<>();
         try {
-            conexao = Conexao.obterConexao();
-            String sql = "SELECT * FROM Fabricante WHERE Nome = ?";
-            ps = conexao.prepareStatement(sql);
-            ps.setString(1, nome);
+            String sql = "SELECT * FROM Fabricante WHERE Nome LIKE ? ORDER BY Nome";
+            try (ResultSet rs = executarConsultaSQL("", "Fabricante", sql, "%" + nome + "%")) {
 
-            rs = ps.executeQuery();
-
-            if (rs.next()) {
-                Fabricante fabricante = new Fabricante();
-                fabricante.fabricanteID = rs.getInt("FabricanteID");
-                fabricante.nome = rs.getString("Nome");
-                fabricante.localizacao = rs.getString("Localizacao");
-                fabricante.descricao = rs.getString("Descricao");
-                return fabricante;
-            } else {
-                return null;
+                while (rs.next()) {
+                    Fabricante fabricante = new Fabricante();
+                    fabricante.fabricanteID = rs.getInt("FabricanteID");
+                    fabricante.nome = rs.getString("Nome");
+                    fabricante.localizacao = rs.getString("Localizacao");
+                    fabricante.descricao = rs.getString("Descricao");
+                    fabricantes.add(fabricante);
+                }
             }
         } catch (SQLException erro) {
-            System.err.printf("Erro ao consultar o Fabricante: %s%n", erro.getMessage());
-            return null;
-        } finally {
-            Conexao.fecharConexao(conexao);
+            System.err.printf("Erro ao consultar os Fabricantes por nome: %s%n", erro.getMessage());
         }
+        return fabricantes;
     }
 
-    public boolean consultarFabricante(int fabricanteID) throws SQLException {
-        Connection conexao = null;
-
+    public Fabricante consultarFabricante(int fabricanteID) throws SQLException {
         try {
-            conexao = Conexao.obterConexao();
             String sql = "SELECT * FROM Fabricante WHERE FabricanteID=?";
-            PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setInt(1, fabricanteID);
-            ResultSet rs = ps.executeQuery();
+            try (ResultSet rs = executarConsultaSQL("", "Fabricante", sql, fabricanteID)) {
 
-            if (!rs.isBeforeFirst()) {
-                System.err.println("Fabricante não encontrado!");
-                return false;
-            } else {
-                while (rs.next()) {
-                    this.fabricanteID = rs.getInt("FabricanteID");
-                    this.nome = rs.getString("Nome");
-                    this.localizacao = rs.getString("Localizacao");
-                    this.descricao = rs.getString("Descricao");
+                if (!rs.isBeforeFirst()) {
+                    System.err.println("Fabricante não encontrado!");
+                    return null;
+                } else {
+                    Fabricante fabricante = new Fabricante();
+                    while (rs.next()) {
+                        fabricante.fabricanteID = rs.getInt("FabricanteID");
+                        fabricante.nome = rs.getString("Nome");
+                        fabricante.localizacao = rs.getString("Localizacao");
+                        fabricante.descricao = rs.getString("Descricao");
+                    }
+                    return fabricante;
                 }
-                return true;
             }
         } catch (SQLException erro) {
             System.err.printf("Erro ao consultar o fabricante: %s%n", erro.getMessage());
-            return false;
-        } finally {
-            Conexao.fecharConexao(conexao);
-        }
-    }
-
-    public boolean atualizarFabricante(String nome) throws SQLException {
-        if (this.fabricanteID == null || this.fabricanteID <= 0) {
-            System.err.println("Erro: FabricanteID inválido para atualização.");
-            return false;
-        }
-
-        Connection conexao = null;
-        try {
-            conexao = Conexao.obterConexao();
-            String sql = "UPDATE Fabricante SET Nome=? WHERE FabricanteID=?";
-            PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setString(1, nome);
-            ps.setInt(2, this.fabricanteID);
-            int totalRegistrosAfetados = ps.executeUpdate();
-            if (totalRegistrosAfetados == 0) {
-                System.err.println("Não foi feita a atualização!");
-            } else {
-                System.err.println("Atualização realizada!");
-            }
-            return true;
-        } catch (SQLException erro) {
-            System.err.printf("Erro ao atualizar o fabricante: %s%n", erro.getMessage());
-            return false;
-        } finally {
-            Conexao.fecharConexao(conexao);
+            return null;
         }
     }
 }
